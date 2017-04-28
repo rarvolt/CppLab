07e4b0a20333513e3a5851d94580ad7ab704a8e8
RARvolt <rarvolt@gmail.com>
2017-04-28 17:39:38 +0200

    Dodano komentarze do klasy Data

--- Data.cpp
+++ Data.cpp
@@ -5,18 +5,34 @@
 #include <iostream>
 #include "Data.h"
 
+/**
+ * Zwraca zawartość składowej zmiennej dzien.
+ */
 int Data::Dzien() const {
     return dzien;
 }
 
+/**
+ * Zwraca zawartość składowej zmiennej miesiac.
+ */
 int Data::Miesiac() const {
     return miesiac;
 }
 
+/**
+ * Zwraca zawartość składowej zmiennej rok.
+ */
 int Data::Rok() const {
     return rok;
 }
 
+/**
+ * Ustawia wartości składowych zmiennych podanymi parametrami.
+ * Następnie dokonuje korekcji daty za pomocą funkcji koryguj().
+ * @param d Dzień
+ * @param m Miesiąc
+ * @param r Rok
+ */
 void Data::Ustaw(int d, int m, int r) {
     dzien = d;
     miesiac = m;
@@ -25,10 +41,18 @@ void Data::Ustaw(int d, int m, int r) {
     koryguj();
 }
 
+/**
+ * Wypisuje datę w formacie dd.mm.rrrr
+ */
 void Data::Wypisz() const {
     std::cout << dzien << "." << miesiac << "." << rok << std::endl;
 }
 
+
+/**
+ * Pobiera od użytkownika dzień, miesiąc i rok,
+ * następnie wywołuje unkcję koryguj().
+ */
 void Data::Wpisz() {
     using std::cout;
     using std::cin;
@@ -42,6 +66,10 @@ void Data::Wpisz() {
     koryguj();
 }
 
+/**
+ * Uproszczona funkcja korekcji daty.
+ * Zamienia nadmiarowe dni w miesiące i nadmiarowe miesiące w lata.
+ */
 void Data::koryguj() {
     if (dzien > 31) {
         miesiac += dzien / 31;
@@ -53,6 +81,11 @@ void Data::koryguj() {
     }
 }
 
+/**
+ * Porównuje dwie daty
+ * @param wzor Data do porównania
+ * @return 1 jeśli równe, 0 jeśli różne
+ */
 int Data::porownaj(const Data &wzor) const {
     return ((dzien == wzor.dzien) &&
             (miesiac == wzor.miesiac) &&
